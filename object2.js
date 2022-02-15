ee84a087090a137de144c33c7ecc05d611768f98
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2022-02-15 12:04:02 +0000

    Create object2.js

---
+++ object2.js
@@ -0,0 +1,4 @@
+const MyCar = {model:"Yaris",colour:"White",brand:"Toyata}; 
+console.log("Model of my car is:",MyCar.model);
+console.log("Colour of my car is:",MyCar.colour); 
+console.log("Brand of my car is:",MyCar.brand);

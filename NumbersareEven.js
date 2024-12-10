4d01bbf04e23be05ccfa5970f17052bb24d804e7
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-12-10 16:04:46 +0000

    Create NumbersareEven.js

---
+++ NumbersareEven.js
@@ -0,0 +1,9 @@
+function EvenNumbers(num) { 
+return num % 2 == 0; 
+} 
+console.log(EvenNumbers(1))
+console.log(EvenNumbers(2))
+console.log(EvenNumbers(3))
+console.log(EvenNumbers(9)) 
+console.log(EvenNumbers(10)) 
+console.log(EvenNumbers(80)) 
